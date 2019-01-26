00f0634b296a5a06e7a25982c900d37741f3fab8
danobi989 <45606091+danobi989@users.noreply.github.com>
2019-01-26 01:49:54 -0500

    lily_whitebox
    Repair for whitebox

---
+++ ProgFund/Assets/Script/PlayerControls.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Make positionCam = 0, in inspector to use follow camera
+//Make positionCam = 1, in inspector to use flip camera
+public class PlayerControls : MonoBehaviour
+{
+
+    public float forwardSpeed = 10;
+    public float sidewaysSpeed = 5;
+    public float jumpForce = 10;
+    public float rotationSpeed = 1;
+    public int positionCam = 1;
+
+    public GameObject pos1;
+    public GameObject pos2;
+    public GameObject pos3;
+    public GameObject pos4;
+
+    public GameObject cam;
+
+    
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Rotate();
+        if (Input.GetKeyDown(KeyCode.E) && positionCam < 4)
+        {
+            positionCam++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && positionCam > 1)
+        {
+            positionCam --;
+        }
+
+        if (positionCam == 1)
+        {
+            cam.transform.position = pos1.transform.position;
+        }
+        if (positionCam == 2)
+        {
+            cam.transform.position = pos2.transform.position;
+            //cam.transform.
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //GET HORIZONTAL MOVEMENT AND ADD THAT TO POSITION
+   
+        Vector3 horizontal = Input.GetAxis("Horizontal") * sidewaysSpeed * transform.right;
+        //GET FORWARD MOVEMENT AND ADD THAT TO POSITION
+
+        Vector3 forward = Input.GetAxis("Vertical") * forwardSpeed * transform.forward;
+
+        rb.AddForce(horizontal + forward);
+        //transform.position += horizontal + forward;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+        }
+    }
+
+    void Rotate()
+    {
+        float deltaRotation = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        Vector3 newLookAtDirection = transform.forward + deltaRotation * transform.right;
+        Vector3 newLookAtPoint = transform.position + newLookAtDirection;
+        transform.LookAt(newLookAtPoint);
+    }
+
+}
---
+++ ProgFund/Assets/Script/spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+    public GameObject item1;
+    public GameObject item2;
+    public GameObject item3;
+    public GameObject item4;
+
+    public int itemchoice;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            itemchoice = Random.Range(1, 5);
+        }
+
+        if (Input.GetKeyDown(KeyCode.G) && itemchoice == 1)
+        {
+            Instantiate(item1, transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.G) && itemchoice == 2)
+        {
+            Instantiate(item2, transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.G) && itemchoice == 3)
+        {
+            Instantiate(item3, transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.G) && itemchoice == 3)
+        {
+            try
+            {
+                Instantiate(item4, transform);
+            }
+            catch
+            {
+                Instantiate(item3, transform);
+                Debug.Log("Error, No item 4");
+            }
+
+        }
+    }
+}
