4dc033441d08f76bbfe1fa941d106e8b99348e2a
Sergio C <45440351+sislegend@users.noreply.github.com>
2019-01-26 06:55:07 -0500

    So close
    aaah

---
+++ light test/Assets/Light.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour {
+
+    public List<GameObject> Lights = new List<GameObject>();
+    public List<GameObject> SPoints = new List<GameObject>();
+    public bool Islit = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject TheShadow in SPoints)
+        {
+            for (int cpt = 0; cpt < Lights.Count; ++cpt)
+            {
+                if (Physics.Raycast(Lights[cpt].transform.position, TheShadow.transform.position - Lights[cpt].transform.position, 5)) //Source.GetComponent<l_source>().lightRange) //checks to see if the raycast is within the light source radius 
+                {
+
+                    Islit = true;
+                }
+                else
+                {
+                    Islit = false;
+                }
+            }
+        }
+    }
+}
---
+++ light test/Assets/LightScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+    //public bool LightHit1 = false;
+    //public bool LightHit2 = false;
+    //public bool LightHit3 = false;
+
+    //public List<GameObject> Items = new List<GameObject>();
+    //public GameObject[] Items;
+    //private bool[] isLit = new bool[2];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
