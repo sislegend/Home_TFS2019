153090eee1fb3f0f7d32e9130b3103f410e403c6
Sergio C <45440351+sislegend@users.noreply.github.com>
2019-01-26 02:12:27 -0500

    Master Skellaton
    Very Basic MM with no Gamestate & Start of Player Movement Script.

---
+++ Master/Assets/Scripts/CMovement.cs
@@ -0,0 +1,35 @@
+﻿// Created by SChiraz 01/2619 - 2:07am
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMovement : MonoBehaviour {
+
+    float PSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * PSpeed);
+
+        var v3 = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+        transform.Rotate(v3 * 50 * Time.deltaTime);
+
+        //transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * PSpeed);
+    }
+}
