52cf88c1efa82c134d40de67b8aa2f4764b238e7
whitelilly883 <45440456+whitelilly883@users.noreply.github.com>
2019-01-26 02:02:01 -0500

    UpdateAt2:01
    got candle

--- ProgFund/Assets/Script/ShadowBehaviour.cs
+++ ProgFund/Assets/Script/ShadowBehaviour.cs
@@ -1,21 +1,49 @@
-﻿using System.Collections;
+﻿// File Created by LFtichar 19/01/2019
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShadowBehaviour : MonoBehaviour
 {
-    public GameObject Target;
+    public GameObject target;
+    public float reduction;
+
+
+    private float lightDistance;
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+        
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.LookAt(Target.transform);
-	}
+        transform.LookAt(target.transform);
+
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit findLightDistance;
+        Ray forwardRay = new Ray(transform.position, Vector3.forward);
+        if (Physics.Raycast(forwardRay, out findLightDistance))
+        {
+            float lightDistance = findLightDistance.distance;
+            
+            if(lightDistance < target.GetComponent<c_LightBehaviour>().L_Radius())
+            {
+                //target.GetComponent<c_LightBehaviour>().L_Radius()
+            }
+        }
+    }
+
+    void FindDistance()
+    {
+
+
+    }
 
 
 
---
+++ ProgFund/Assets/Script/c_LightBehaviour.cs
@@ -0,0 +1,32 @@
+﻿//CREATED BY K.ENNIS 1/26/19 1:19AM
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_LightBehaviour : MonoBehaviour {
+
+ 
+    private float l_Radius;
+    Light p_Candle;
+
+    void Start()
+    {
+        p_Candle = GetComponent<Light>();
+        l_Radius = p_Candle.range;
+    }
+
+    void Update()
+    {
+        p_Candle.range = l_Radius;
+    }
+
+    public float L_Radius()
+    {
+        return l_Radius;
+    }
+
+    public void L_Radius(float change)
+    {
+        l_Radius = change;
+    }
+}
