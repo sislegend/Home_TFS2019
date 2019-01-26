59f3a9ef5844784bba7be57b83fcc9a076491bd2
danobi989 <45606091+danobi989@users.noreply.github.com>
2019-01-26 16:25:21 -0500

    Camera Change
    Camera offset changes

---
+++ GameJam2019_v01/GameJam2019_v01/Assets/Scripts/c_LightBehaviour.cs
@@ -0,0 +1,68 @@
+﻿//CREATED BY K.ENNIS (Loui wuz here) 1/26/19 1:19AM
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_LightBehaviour : MonoBehaviour {
+    
+    public float l_Radius;
+    public float l_RadiusMax = 12;
+    public float l_Intensity;
+    public bool radiusDecreasing;
+    Light p_Candle;
+    public GameObject[] shadow;
+    public RaycastHit shadowRay;
+
+    
+
+    public int timesCoroutineCalled;
+
+    void Start()
+    {
+        p_Candle = GetComponent<Light>();
+        l_Intensity = p_Candle.spotAngle;
+        l_Radius = p_Candle.range;
+        radiusDecreasing = false;
+        timesCoroutineCalled = 0;
+    }
+
+    void Update()
+    {
+        p_Candle.range = l_Radius;
+        p_Candle.spotAngle = l_Intensity;
+
+        if (radiusDecreasing)
+        {
+            l_Radius -= Time.deltaTime;
+            GameObject.FindGameObjectWithTag("Actual Shadow").transform.localScale += new Vector3(.5f, -l_Radius, 0);
+        }
+        if (!radiusDecreasing)
+        {
+            if (l_Radius < l_RadiusMax)
+            {
+                l_Radius += Time.deltaTime;
+            }
+        }
+        if(l_Radius <= 0)
+        {
+            p_Candle.intensity = 0;
+            GameObject.FindGameObjectWithTag("flicker").SetActive(false);
+           
+        }
+    }
+    private void OnTriggerEnter(Collider c)
+    {
+        if (c.gameObject.tag == "Shadow")
+        {
+            radiusDecreasing = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider c)
+    {
+        if (c.gameObject.tag == "Shadow")
+        {
+            radiusDecreasing = false;
+        }
+    }
+}
