1ecaf7021889577c2600971e2f2da77e9d52213d
Sergio C <45440351+sislegend@users.noreply.github.com>
2019-01-26 05:22:49 -0500

    Scene 1
    Ey

---
+++ GameJam2019_v01/Assets/Scripts/MMenu.cs
@@ -0,0 +1,21 @@
+﻿// Created by SChiraz 1/26/19 - 12:29am
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MMenu : MonoBehaviour {
+
+    public void Load()
+    {
+        // Input Fade + Sound
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public  void Quit()
+    {
+        // Check GameState
+        Application.Quit();
+    }
+}
