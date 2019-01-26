c0fb0a285dc2f0c4437f1ecab9d4890b47bce433
ATepylo <45440409+ATepylo@users.noreply.github.com>
2019-01-26 06:58:45 -0500

    Added the lamp and candle prefabs and scripts
    basic lighting theme for the game

---
+++ GameJam2019_v01/Assets/Scripts/c_LightBehaviour.cs
@@ -0,0 +1,25 @@
+﻿//CREATED BY K.ENNIS 1/26/19 1:19AM
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_LightBehaviour : MonoBehaviour {
+
+ 
+    public float l_Radius;
+    public float l_Intensity;
+    Light p_Candle;
+
+    void Start()
+    {
+        p_Candle = GetComponent<Light>();
+        l_Intensity = p_Candle.spotAngle;
+        l_Radius = p_Candle.range;
+    }
+
+    void Update()
+    {
+        p_Candle.range = l_Radius;
+        p_Candle.spotAngle= l_Intensity;
+    }
+}
