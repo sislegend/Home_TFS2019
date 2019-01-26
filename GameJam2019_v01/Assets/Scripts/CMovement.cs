99278e55af4fc2edf89a23fb00f33f98014dbbc5
Sergio C <45440351+sislegend@users.noreply.github.com>
2019-01-26 06:58:52 -0500

    o!
    O!O

---
+++ GameJam2019_v01/Assets/Scripts/CMovement.cs
@@ -0,0 +1,34 @@
+﻿// Created by SChiraz 01/26/19 - 2:07am
+// Edited by SChiraz 01/26/19 - 3:49am
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMovement : MonoBehaviour {
+
+    float PSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * PSpeed);
+
+        var v3 = new Vector3(0, Input.GetAxis("Horizontal"), 0);
+        transform.Rotate(v3 * 50 * Time.deltaTime);
+    }
+}
---
+++ Master/Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public enum GameState { play, pause, end }
+    public GameState CurrentGState = GameState.play;
+
+    public CMovement ThePlayer;
+
+    public GameObject PauseUI;
+
+    private static GameManager Instance;
+
+    public static GameManager Get()
+    {
+        return Instance;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Instance)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (CurrentGState == GameState.play)
+            {
+                CurrentGState = GameState.pause;
+            }
+            else if (CurrentGState == GameState.pause)
+            {
+                CurrentGState = GameState.play;
+            }
+        }
+
+        switch (CurrentGState)
+        {
+            case GameState.play:
+                Play();
+                break;
+
+            case GameState.pause:
+                Pause();
+                break;
+
+            case GameState.end:
+                End();
+                break;
+        }
+
+    }
+
+    void Play()
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    void Pause()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void End()
+    {
+        Time.timeScale = 0;
+    }
+}
