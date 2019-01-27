fc76392b99f8cbcf4b9e0dbc2eff844423eeb3f9
Sergio C <45440351+sislegend@users.noreply.github.com>
2019-01-27 03:07:19 -0500

    Alec's
    !

---
+++ Alec's added/GameJam2019_v01/Assets/Scripts/GameManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public enum GameState { play, pause, end }
+    public GameState CurrentGState = GameState.play;
+
+    //public CMovement ThePlayer;
+
+    public GameObject[] PauseUI;
+
+    private static GameManager Instance;
+
+    public static GameManager Get()
+    {
+        return Instance;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (Instance)
+        {
+            Destroy(Instance);
+        }
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (CurrentGState == GameState.play)
+            {
+                CurrentGState = GameState.pause;
+            }
+            else if (CurrentGState == GameState.pause)
+            {
+                CurrentGState = GameState.play;
+            }
+        }
+
+        switch (CurrentGState)
+        {
+            case GameState.play:
+                Play();
+                break;
+
+            case GameState.pause:
+                Pause();
+                break;
+
+            case GameState.end:
+                End();
+                break;
+        }
+
+    }
+
+    void Play()
+    {
+        PauseUI[1].SetActive(false);
+        PauseUI[0].SetActive(false);
+       
+        Time.timeScale = 1;
+    }
+
+    void Pause()
+    {
+        PauseUI[1].SetActive(true);
+        PauseUI[0].SetActive(true);
+        PauseUI[2].SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void End()
+    {
+        Time.timeScale = 0;
+    }
+}
---
+++ Alec's added/GameJam2019_v01/Assets/Scripts/c_LightBehaviourPulse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_LightBehaviourPulse : MonoBehaviour {
+
+
+    public float l_Radius;
+    public float l_Intensity;
+    Light p_Candle;
+
+    void Start()
+    {
+        p_Candle = GetComponent<Light>();
+        l_Intensity = p_Candle.intensity;
+        l_Radius = p_Candle.range;
+    }
+
+    void Update()
+    {
+        Random.Range(.042f, 1.34f);
+        p_Candle.range = l_Radius;
+        p_Candle.intensity = l_Intensity;
+        var amplitude = Mathf.PingPong(Time.time, 4);
+
+        amplitude = amplitude / 2 * 0.5f + 0.5f;
+        p_Candle.intensity = l_Intensity * amplitude;
+    }
+}
