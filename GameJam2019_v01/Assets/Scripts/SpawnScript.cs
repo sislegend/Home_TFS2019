81d5e3cfba96c3c137f10cd678da26a3eddfd531
Revolver1388 <45246957+Revolver1388@users.noreply.github.com>
2019-01-26 20:19:19 -0500

    NEWMaster

---
+++ GameJam2019_v01/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public GameObject shadow;
+
+    private int shadowNum = 0;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Animator>().Play("idle");
+        shadow.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider c)
+    {
+        if(c.gameObject.tag == "Candle")
+        {
+            if (shadowNum == 0)
+            {
+                SpawnShadow();
+                gameObject.GetComponent<Animator>().Play("rockingHorse");
+                shadowNum++;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider c) 
+    {
+        if (c.gameObject.tag == "Candle")
+        {
+           // gameObject.GetComponent<Animator>().Play("idle");
+        }
+    }
+    public void SpawnShadow()
+    {
+        Instantiate(shadow, spawnPoint.transform.position, Quaternion.identity);
+    }
+}
---
+++ ProgFund/c_LightBehaviour.cs
@@ -0,0 +1,22 @@
+﻿//CREATED BY K.ENNIS 1/26/19 1:19AM
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_LightBehaviour : MonoBehaviour {
+
+ 
+    public float l_Radius;
+    Light p_Candle;
+
+    void Start()
+    {
+        p_Candle = GetComponent<Light>();
+        l_Radius = p_Candle.range;
+    }
+
+    void Update()
+    {
+        p_Candle.range = l_Radius;
+    }
+}
