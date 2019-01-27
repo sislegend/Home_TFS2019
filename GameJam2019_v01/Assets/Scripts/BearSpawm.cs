7264a2604addb433cfee34c8395b328b806577fb
Sergio C <45440351+sislegend@users.noreply.github.com>
2019-01-27 08:31:35 -0500

    Build1
    Build1

---
+++ GameJam2019_v01/Assets/Scripts/BearSpawm.cs
@@ -0,0 +1,24 @@
+﻿//CREATED SChiraz 1/27/19 3:39AM
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearSpawm : MonoBehaviour {
+
+    public GameObject MrPickles;
+    public GameObject[] sp_Bear;
+
+	// Use this for initialization
+	void Start () {
+
+        int randomIndex = UnityEngine.Random.Range(0, sp_Bear.Length);
+        Instantiate(MrPickles, sp_Bear[randomIndex].transform);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GameJam2019_v01/Assets/Scripts/IntroVideoCall.cs
@@ -0,0 +1,37 @@
+﻿//created by Lilly 1/26/2019 11.33pm
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class IntroVideoCall : MonoBehaviour
+{
+    
+    public int n_videoLength;
+    public int n_NextScene;
+
+    private float n_videoPlayedTime;
+    private bool n_endVideo;
+
+	// Use this for initialization
+	void Start ()
+    {
+        n_videoPlayedTime = 0;
+        n_endVideo = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        n_videoPlayedTime += Time.deltaTime;
+        if (n_videoPlayedTime > n_videoLength)
+        {
+            n_endVideo = true;
+        }
+
+        if (n_endVideo)
+        {
+            SceneManager.LoadScene(n_NextScene, LoadSceneMode.Single);
+        }
+    }
+}
