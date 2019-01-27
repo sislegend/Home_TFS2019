3b1cc1f3886f95701103400dd9398ed83d5dc7b6
Sergio C <45440351+sislegend@users.noreply.github.com>
2019-01-26 23:09:58 -0500

    Updated Version
    SChiraz

---
+++ GameJam2019_v01/Assets/Scripts/SceneOneToTwo.cs
@@ -0,0 +1,19 @@
+﻿//Created by Lilly 1/26/2019 9.20pm
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+
+public class SceneOneToTwo : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider collider)
+    {  
+        if (collider.gameObject.tag == "Player" && !GameObject.FindGameObjectWithTag("Actual Shadow").active)
+        {
+            SceneManager.LoadScene(2, LoadSceneMode.Single);
+        }
+    }
+}
